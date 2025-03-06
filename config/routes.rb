1982d77f5a46239bf1db704764179fa06afb824e
EC2 Default User <ec2-user@ip-172-31-1-93.ap-northeast-1.compute.internal>
2025-03-06 06:55:19 +0000

    解答に沿って軽微修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update]
-  resource :favorite, only: [:create, :destroy]
+  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
